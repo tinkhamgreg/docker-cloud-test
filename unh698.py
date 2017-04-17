e3017872d7ada1ffe506fa223939316d70a7ee11
Tinkhamgreg <gjtinkham@wildcats.unh.edu>
2017-04-17 23:31:29 +0000

    removed name from render function

--- unh698.py
+++ unh698.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'UNH698 Website', render_template('index.html', name=name)
+    return 'UNH698 Website', render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port = 8080)
