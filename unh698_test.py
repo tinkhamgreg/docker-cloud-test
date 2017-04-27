b9f1dc177ad93429ee4b627090681c1bd6ac1cf7
Greg Tinkham <gtinkham@gmail.com>
2017-04-27 06:13:10 +0000

    tag 14

--- unh698_test.py
+++ unh698_test.py
@@ -9,7 +9,7 @@ class FlaskrTestCase(unittest.TestCase):
         self.app = unh698.app.test_client()
 
     def tearDown(self):
-        pass
+         pass
 
     def test_home_page(self):
         # Render the / path of the website
@@ -18,16 +18,16 @@ class FlaskrTestCase(unittest.TestCase):
         assert b'UNH698 Website' in rv.data
 
     def test_link_to_my_page(self):
-   	 rv = self.app.get('/')  
-        # Search the page contents for the link to your topic page 
+        rv = self.app.get('/')
+        # Search the page contents for the link to your topic page
         # Replace xxxxxxxxxxxx with text you'd expect to see on your main page that links to your subpage
-        assert b'RL Models' in rv.data 
+        assert b'UNH698 Website' in rv.data
 
     def test_my_topic(self):
         # Replace '/' with the page path you want to make
-        rv = self.app.get('/')  
+        rv = self.app.get('/rlmodel')
         # Replace UNH698 Website with the text you expect to see on you topic page
-        assert b'UNH698 Website' in rv.data
+        assert b'RL Models' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
