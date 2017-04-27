a2421679637173c23fb3d284c3ab6ba65348af37
Greg Tinkham <gtinkham@gmail.com>
2017-04-27 01:59:26 +0000

    added two webpages and templates

--- unh698.py
+++ unh698.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'UNH698 Website'
+    return render_template('index.html')
+
+@app.route('/rlmodels.html
+def rlmodel():
+	return render_template('rlmodel.html')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
